fa2401582d10147b77aaba6a35acdf515ee247a2
Akshat <akshatsaraswat03@gmail.com>
2020-11-12 06:39:21 +0530

    adding links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Homepage from './Pages/Homepage'
+import Productpage from './Pages/Productpage'
 
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Homepage />
+          <Route exact path="/" component={Homepage} />
+          <Route path='/product/:id' component={Productpage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
