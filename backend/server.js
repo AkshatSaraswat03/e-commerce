11ef1e9febe328a7230c367f18b1c74a8898e1eb
Akshat <akshatsaraswat03@gmail.com>
2020-11-12 13:04:31 +0530

    backend init

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+
+
+
+
+const PORT = process.env.PORT || 6000
+
+app.listen(PORT, console.log("server running on http://localhost:6000"))
