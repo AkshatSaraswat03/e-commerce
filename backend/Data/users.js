d51a9c2d1563103a9c88f97db9eb09aa491be3a4
Akshat <akshatsaraswat03@gmail.com>
2020-11-15 14:41:56 +0530

    added temporary users

---
+++ backend/Data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@dukaan.com',
+    password: xxx,
+    isAdmin: true
+  },
+  {
+    name: 'Micheal King',
+    email: 'micheal@dukaan.com',
+    password: xxx,
+  },
+  {
+    name: 'Trevor Sass',
+    email: 'trevor@dukaan.com',
+    password: xxx,
+  },
+]
