569134d926a8511b52148bfb5e53e2f404830289
Akshat <akshatsaraswat03@gmail.com>
2020-11-15 16:01:39 +0530

    data entered on server

--- backend/Data/users.js
+++ backend/Data/users.js
@@ -1,18 +1,22 @@
+const bcrypt = require('bcryptjs')
+
 const users = [
   {
     name: 'Admin User',
     email: 'admin@dukaan.com',
-    password: xxx,
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true
   },
   {
     name: 'Micheal King',
     email: 'micheal@dukaan.com',
-    password: xxx,
+    password: bcrypt.hashSync('123456', 10),
   },
   {
     name: 'Trevor Sass',
     email: 'trevor@dukaan.com',
-    password: xxx,
+    password: bcrypt.hashSync('123456', 10),
   },
 ]
+
+module.exports = users
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,6 +12,11 @@ const reviewSchema = mongoose.Schema({
 
 
 const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
   name: {
     type: String,
     required: true
@@ -28,7 +33,7 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  decription: {
+  description: {
     type: String,
     required: true
   },
