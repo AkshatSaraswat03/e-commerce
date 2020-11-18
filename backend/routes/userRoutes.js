63789be9f03214fb3f9d6cbc34090a15ebf0f1da
Akshat <akshatsaraswat03@gmail.com>
2020-11-18 19:38:04 +0530

    login route and protect middleware done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const User = require('../models/userModel')
 const bcrypt = require('bcryptjs')
 const generateToken = require('../utils/generateToken')
+const protect = require('../middlewares/authMiddleware')
+
 
 // to authenticate a user. validate email/password and get a token
 //public
@@ -25,5 +27,30 @@ router.post('/login', asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password')
   }
 }))
+
+
+// to get a logged in user. get a token
+//private
+router.get('/profile', protect, asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  console.log(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found !')
+  }
+
+  //res.send('success')
+
+}))
+
+
 
 module.exports = router
