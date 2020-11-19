575b7a12e903f275d2e63e522f35eb9dbe05095e
Akshat <akshatsaraswat03@gmail.com>
2020-11-19 19:34:06 +0530

    routes for update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -53,6 +53,41 @@ router.get('/profile', protect, asyncHandler(async (req, res) => {
 
 
 
+// to update a user profile.
+// PUT request
+// private
+router.put('/profile', protect, asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  //console.log(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.user.password) {
+      user.password = req.user.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found !')
+  }
+
+
+}))
+
+
+
+
 // to register a new user
 //public
 router.post('/', asyncHandler(async (req, res) => {
