c15ea3984db3b7c3f75e03ed0eebd23cf457ab0f
Akshat <akshatsaraswat03@gmail.com>
2020-11-20 12:43:26 +0530

    checkout steps component

--- frontend/src/Pages/Shippingpage.js
+++ frontend/src/Pages/Shippingpage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const Shippingpage = ({ history }) => {
@@ -26,6 +27,7 @@ const Shippingpage = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h2>Shipping</h2>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
