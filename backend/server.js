7b7377f135c42213656e858770d274ad4750dde9
Akshat <akshatsaraswat03@gmail.com>
2020-11-22 22:37:50 +0530

    adding review route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
@@ -12,6 +13,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
