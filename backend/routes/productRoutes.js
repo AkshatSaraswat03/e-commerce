ccb5a20b42204d5256afc8329a02a358d5b0b9a2
Akshat <akshatsaraswat03@gmail.com>
2020-11-22 09:25:48 +0530

    add routes for creating and updating product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -55,5 +55,61 @@ router.delete('/:id', protect, admin, asyncHandler(async (req, res) => {
   }
 }))
 
+//create a product
+//private
+router.post('/', protect, admin, asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample Brand',
+    category: 'Sample Category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample Description'
+  })
+
+  const createdProduct = await product.save()
+  res.json(201).json(createdProduct)
+
+}))
+
+//update a product
+//private
+router.put('/:id', protect, admin, asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+
+  } else {
+    res.status(404)
+    throw new Error('Product not Found')
+  }
+
+
+}))
+
 
 module.exports = router
